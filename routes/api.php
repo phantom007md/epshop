42d0d6150b9d5a5aba04522a8fb692e65664864d
phantom <mohamad.d007@gmail.com>
2018-03-12 11:20:09 +0330

    routing WIP

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,67 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+// Products Routes.
+Route::get('/products', 'ProductController@index');
+
+//Route::get('products/create', 'ProductController@create');
+Route::post('products', 'ProductController@store');
+
+Route::get('products/{product}', 'ProductController@show');
+
+//Route::get('products/{product}/edit', 'ProductController@edit');
+Route::patch('products/{product}', 'ProductController@update');
+
+Route::delete('products/{product}', 'ProductController@destroy');
+
+
+
+// Guaranties Routes.
+Route::get('guaranties', 'GuarantyController@index')->name('guaranties.index');
+
+//Route::get('guaranties/create', 'GuarantyController@create');
+Route::post('guaranties', 'GuarantyController@store');
+
+Route::get('guaranties/{guaranty}', 'GuarantyController@show');
+
+//Route::get('guaranties/{guaranty}/edit', 'GuarantyController@edit');
+Route::patch('guaranties/{guaranty}', 'GuarantyController@update');
+
+Route::delete('guaranties/{guaranty}', 'GuarantyController@destroy');
+
+
+
+// Users Routes.
+Route::get('users', 'UserController@index')->name('users.index');
+
+//Route::get('users/create', 'UserController@create');
+Route::post('users', 'UserController@store');
+
+Route::get('users/{user}', 'UserController@show');
+
+//Route::get('users/{user}/edit', 'UserController@edit');
+Route::patch('users/{user}', 'UserController@update');
+
+Route::delete('users/{user}', 'UserController@destroy');
+
+
+
+// Comments Routes.
+Route::get('comments', 'CommentController@index')->name('comments.index');
+
+//Route::get('comments/create', 'CommentController@create');
+Route::post('comments', 'CommentController@store');
+
+Route::get('comments/{comment}', 'CommentController@show');
+
+//Route::get('comments/{comment}/edit', 'CommentController@edit');
+Route::patch('comments/{comment}', 'CommentController@update');
+
+Route::delete('comments/{comment}', 'CommentController@destroy');
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,57 @@ Route::get('/', 'ProductController@index')->name('home');
 
 Route::get('products/create', 'ProductController@create');
 Route::post('products', 'ProductController@store');
-Route::delete('products/{product}', 'ProductController@destroy');
+
+Route::get('products/{product}', 'ProductController@show');
+
+Route::get('products/{product}/edit', 'ProductController@edit');
+Route::post('products/{product}/update', 'ProductController@update');
+
+Route::get('products/{product}/delete', 'ProductController@destroy');
+
+
 
 // Guaranties Routes.
 Route::get('guaranties', 'GuarantyController@index')->name('guaranties.index');
+
 Route::get('guaranties/create', 'GuarantyController@create');
 Route::post('guaranties', 'GuarantyController@store');
-Route::delete('guaranties/{guaranty}', 'GuarantyController@destroy');
 
-// Admins Routes.
-Route::get('admins', 'AdminController@index')->name('admins.index');
-Route::get('admins/create', 'AdminController@create');
-Route::post('admins', 'AdminController@store');
-Route::delete('admins/{admin}', 'AdminController@destroy');
+Route::get('guaranties/{guaranty}', 'GuarantyController@show');
+
+Route::get('guaranties/{guaranty}/edit', 'GuarantyController@edit');
+Route::post('guaranties/{guaranty}/update', 'GuarantyController@update');
+
+Route::get('guaranties/{guaranty}/delete', 'GuarantyController@destroy');
+
+
 
 // Users Routes.
 Route::get('users', 'UserController@index')->name('users.index');
+
 Route::get('users/create', 'UserController@create');
 Route::post('users', 'UserController@store');
-Route::get('users/{user}', 'UserController@destroy');
+
+Route::get('users/{user}', 'UserController@show');
+
+Route::get('users/{user}/edit', 'UserController@edit');
+Route::post('users/{user}/update', 'UserController@update');
+
+Route::get('users/{user}/delete', 'UserController@destroy');
+
+
 
 // Comments Routes.
 Route::get('comments', 'CommentController@index')->name('comments.index');
+
 Route::get('comments/create', 'CommentController@create');
 Route::post('comments', 'CommentController@store');
-Route::get('comments/{comment}', 'CommentController@destroy');
+
+Route::get('comments/{comment}', 'CommentController@show');
+
+Route::get('comments/{comment}/edit', 'CommentController@edit');
+Route::post('comments/{comment}/update', 'CommentController@update');
+
+Route::get('comments/{comment}/delete', 'CommentController@destroy');
+
+
